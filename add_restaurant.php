208f93d3c3a41be0b0206530ff3c1562a59a1a04
Kaiyuan Hu <cleryhu@gmail.com>
2017-05-03 14:39:51 -0400

    update add_restaurant.php

--- add_restaurant.php
+++ add_restaurant.php
@@ -30,10 +30,16 @@ $id5 = $_POST['id5'];
 $id6 = $_POST['id6'];
 $id7 = $_POST['id7'];
 
+echo"step 1";
+
 $sql1 = "SELECT @id := (max(Restaurant_ID) + 1) from Restaurant_info;";
 $conn->query($sql1);
+
+echo"step 2";
 $sql2 = "INSERT into Restaurant_info values(@id, $id1, $id2, $id3, $id4, $id5, $id6, $id7);";
 $conn->query($sql2);
+
+echo"step 3";
 
 $sql3 = = "SELECT * FROM Restaurant_info WHERE Restaurant_Name LIKE '%$id1%';";
 $result = $conn->query($sql3);
